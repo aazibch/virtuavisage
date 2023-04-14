ff9cbc5e91e5ee12c513c9b9e2d481d8cf4bfa6f
Aazib Chaudhry <aazibch47@gmail.com>
2023-04-15 01:56:52 +0500

    Update production urls

--- server/constants/index.js
+++ server/constants/index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 let clientUrl = 'http://localhost:5173';
 
 if (process.env.NODE_ENV === 'production') {
-  clientUrl = '';
+  clientUrl = 'https://virtuavisage.onrender.com';
 }
 
 let apiUrl = 'http://localhost:8080';
 
 if (process.env.NODE_ENV === 'production') {
-  apiUrl = '';
+  apiUrl = 'https://virtuavisage-api.onrender.com';
 }
 
 module.exports = { clientUrl, apiUrl };
