49a1847ec6d1b75110e0d9a5d7d559aef81885cd
Aazib Chaudhry <aazibch47@gmail.com>
2023-04-15 04:10:44 +0500

    Modify

--- server/app.js
+++ server/app.js
@@ -5,11 +5,10 @@ const MongoStore = require('connect-mongo');
 const express = require('express');
 const app = express();
 
-app.set('trust proxy', 1);
+app.enable('trust proxy');
 const { clientUrl } = require('./constants');
 require('./middleware/auth');
 
-console.log('clientUrl', clientUrl);
 app.use(cors({ credentials: true, origin: clientUrl }));
 app.options('*', cors({ credentials: true, origin: clientUrl }));
 app.use(express.json({ limit: '50mb' }));
@@ -21,7 +20,8 @@ app.use(
     cookie: {
       maxAge: 24 * 60 * 60 * 100 * process.env.SESSION_AGE,
       sameSite: 'none',
-      secure: true
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true
     },
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_SESSION_URL })
   })
