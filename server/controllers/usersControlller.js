b2455cbb55fc285b2ea35c6556cb3759decac381
Aazib Chaudhry <aazibch47@gmail.com>
2023-04-15 05:53:51 +0500

    modify

--- server/controllers/usersControlller.js
+++ server/controllers/usersControlller.js
@@ -4,7 +4,7 @@ const catchAsync = require('../middleware/catchAsync');
 
 exports.getMe = catchAsync(async (req, res, next) => {
   let user = await User.findById(req.user);
-  console.log('[getMe] user', user);
+  console.log('[getMe] req.user', req.user);
 
   if (!user)
     return res.status(200).json({
